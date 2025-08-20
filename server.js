614826df878bcb4a4c790f6e528cf9c636e5f385
getnextapp <runsoffhopesanddreams@gmail.com>
2025-08-19 23:05:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-let onlineUsers = 0;
-
 io.on("connection", (socket) => {
   onlineUsers++;
   io.emit("online-count", onlineUsers);
 
-  // ... existing chat logic ...
+  if (waitingUser) {
+    // pair users
+    socket.partner = waitingUser;
+    waitingUser.partner = socket;
 
+    socket.emit("message", "You are now connected to a stranger!");
+    waitingUser.emit("message", "You are now connected to a stranger!");
+    waitingUser = null;
+  } else {
+    waitingUser = socket;
+    socket.emit("message", "Waiting for a stranger...");
+  }
+
+  // text message
+  socket.on("message", (msg) => {
+    if (socket.partner) {
+      socket.partner.emit("message", msg);
+    }
+  });
+
+  // image message 👇 (this is where you add it)
+  socket.on("image", (imgData) => {
+    if (socket.partner) socket.partner.emit("image", imgData);
+  });
+
+  // disconnect
   socket.on("disconnect", () => {
     onlineUsers--;
     io.emit("online-count", onlineUsers);
