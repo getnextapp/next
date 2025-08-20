d643973ac255dddec8a25eb37bc3c3e21b6675f4
getnextapp <runsoffhopesanddreams@gmail.com>
2025-08-20 01:58:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,60 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+let waitingUser = null;
+let onlineUsers = 0;
+
+app.use(express.static(path.join(__dirname, "public")));
+
 io.on("connection", (socket) => {
   onlineUsers++;
   io.emit("online-count", onlineUsers);
 
   if (waitingUser) {
-    // pair users
+    // Pair users
     socket.partner = waitingUser;
     waitingUser.partner = socket;
 
-    socket.emit("message", "You are now connected to a stranger!");
-    waitingUser.emit("message", "You are now connected to a stranger!");
+    socket.emit("message", { sender: "system", text: "Connected to a stranger!" });
+    waitingUser.emit("message", { sender: "system", text: "Connected to a stranger!" });
     waitingUser = null;
   } else {
     waitingUser = socket;
-    socket.emit("message", "Waiting for a stranger...");
+    socket.emit("message", { sender: "system", text: "Waiting for a stranger..." });
   }
 
-  // text message
+  // Handle text
   socket.on("message", (msg) => {
     if (socket.partner) {
-      socket.partner.emit("message", msg);
+      socket.partner.emit("message", { sender: "stranger", text: msg });
     }
   });
 
-  // image message 👇 (this is where you add it)
+  // Handle image
   socket.on("image", (imgData) => {
-    if (socket.partner) socket.partner.emit("image", imgData);
+    if (socket.partner) {
+      socket.partner.emit("image", imgData);
+    }
   });
 
-  // disconnect
+  // Disconnect
   socket.on("disconnect", () => {
     onlineUsers--;
     io.emit("online-count", onlineUsers);
 
     if (socket.partner) {
-      socket.partner.emit("partner-left");
+      socket.partner.emit("message", { sender: "system", text: "Stranger disconnected." });
       socket.partner.partner = null;
     }
     if (waitingUser === socket) waitingUser = null;
   });
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
